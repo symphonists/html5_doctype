2b0d02733c48214b7dafac2f06c4d93da5c497c3
michael-e <info@michael-eichelsdoerfer.de>
2011-01-25 14:01:01 +0100

    Parse only if $context['output'] exists and it's an HTML document.
    This fixes problems with Symphony error pages coming up blank, and it prohibits any parsing attempts for other document types.

--- extension.driver.php
+++ extension.driver.php
@@ -23,26 +23,29 @@
 				),
 			);
 		}
-		
+
 		public function parse_html($context) {
-			$html = $context['output'];
-			
-			// Split the HTML output into two variables:
-			// $html_doctype contains the first four lines of the HTML document
-			// $html_doc contains the rest of the HTML document
-			$html_array = explode("\n", $html, 5);
-			$html_doc = array_pop($html_array);
-			$html_doctype = implode("\n", $html_array);
-			
-			// Parse the doctype to convert XHTML syntax to HTML5
-			$html_doctype = preg_replace("/<!DOCTYPE [^>]+>/", "<!DOCTYPE html>", $html_doctype);
-			$html_doctype = preg_replace('/(<html ).*(lang=\"[a-z]+\").*>/i', '\\1\\2>', $html_doctype);
-			$html_doctype = preg_replace('/<meta http-equiv=\"Content-Type\" content=\"text\/html; charset=(.*[a-z0-9-])\" \/>/i', '<meta charset="\\1" />', $html_doctype);
+			// Parse only if $context['output'] exists and it's an HTML document
+			if(substr($context['output'], 0, 14) == '<!DOCTYPE html') {
+				$html = $context['output'];
 
-			// Concatenate the fragments into a complete HTML5 document
-			$html = $html_doctype . "\n" . $html_doc;
+				// Split the HTML output into two variables:
+				// $html_doctype contains the first four lines of the HTML document
+				// $html_doc contains the rest of the HTML document
+				$html_array = explode("\n", $html, 5);
+				$html_doc = array_pop($html_array);
+				$html_doctype = implode("\n", $html_array);
 
-			$context['output'] = $html;
+				// Parse the doctype to convert XHTML syntax to HTML5
+				$html_doctype = preg_replace("/<!DOCTYPE [^>]+>/", "<!DOCTYPE html>", $html_doctype);
+				$html_doctype = preg_replace('/(<html ).*(lang=\"[a-z]+\").*>/i', '\\1\\2>', $html_doctype);
+				$html_doctype = preg_replace('/<meta http-equiv=\"Content-Type\" content=\"text\/html; charset=(.*[a-z0-9-])\" \/>/i', '<meta charset="\\1" />', $html_doctype);
+
+				// Concatenate the fragments into a complete HTML5 document
+				$html = $html_doctype . "\n" . $html_doc;
+
+				$context['output'] = $html;
+			}
 		}
 
 	}
